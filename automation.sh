d219d8348b159109ecf9fb4cdcbe783368ce15ec
Madhuriyama <38128763+Madhuriyama@users.noreply.github.com>
2021-10-20 20:54:21 +0530

    removed unecessary spaces

--- automation.sh
+++ automation.sh
@@ -7,12 +7,10 @@ sudo systemctl enable apache2
 TimeCreated=$(date +%d%m%Y-%H%M%S)
 
 sudo tar -zcvf "/tmp/madhuri-httpd-logs-$TimeCreated.tar.gz" /var/log/apache2/
-<<<<<<< HEAD
 sudo aws s3 cp /tmp/madhuri-httpd-logs-$TimeCreated.tar.gz s3://$s3bucketname/madhuri-httpd-logs-$TimeCreated.tar
-=======
-sudo aws s3 cp /tmp/madhuri-httpd-logs-$TimeCreated.tar.gz s3://$s3bucketname/madhuri-httpd-logs-$TimeCreated.tar
+
 
 sudo touch /etc/cron.d/automation
 echo "30 18 * * * sh /root/Automation_Project/automation.sh" > /etc/cron.d/automation
 sudo /usr/bin/crontab /etc/cron.d/automation
->>>>>>> dev
+
